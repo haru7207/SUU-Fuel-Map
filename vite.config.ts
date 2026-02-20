ffc5a4a661ec05143ba2238e578b3bdff0ba201e
haru7207 <51056585+haru7207@users.noreply.github.com>
2026-02-20 08:44:43 -0700

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/SUU-Fuel-Map/',
+  // base 설정이 없어야 Vercel에서 정상 작동합니다.
 })
